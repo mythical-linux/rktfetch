22f9653242ad9162ebba0120828c7254ec3ca912
Maciej Barć <xgqt@riseup.net>
2021-03-13 01:58:52 +0100

    rktfetch/main.rkt: wrap in main module
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- rktfetch/main.rkt
+++ rktfetch/main.rkt
@@ -15,36 +15,38 @@
 ;; Terminal
 ;; Uptime -- BSD
 
-;; Gather info and output
-(let*
-    (
-     [user    (get-user)]
-     [host    (gethostname)]
-     [cpu     (get-cpu)]
-     [desktop (get-environment)]
-     [device  (get-device)]
-     [editor  (get-editor)]
-     [os      (string-titlecase (symbol->string (system-type 'os)))]
-     [distro  (get-distro os)]
-     [kernel  (get-kernel os)]
-     [memory  (get-memory os)]
-     [pkg     (get-pkgmanager os)]
-     [shell   (get-shell)]
-     [uptime  (get-uptime os)]
+(module+ main
+  ;; Gather info and output
+  (let*
+      (
+       [user    (get-user)]
+       [host    (gethostname)]
+       [cpu     (get-cpu)]
+       [desktop (get-environment)]
+       [device  (get-device)]
+       [editor  (get-editor)]
+       [os      (string-titlecase (symbol->string (system-type 'os)))]
+       [distro  (get-distro os)]
+       [kernel  (get-kernel os)]
+       [memory  (get-memory os)]
+       [pkg     (get-pkgmanager os)]
+       [shell   (get-shell)]
+       [uptime  (get-uptime os)]
+       )
+    (display
+     (string-append
+      user "@" host       "\n"
+      "CPU:     " cpu     "\n"
+      "DESKTOP: " desktop "\n"
+      "DEVICE:  " device  "\n"
+      "DISTRO:  " distro  "\n"
+      "EDITOR:  " editor  "\n"
+      "KERNEL:  " kernel  "\n"
+      "PKGS:    " pkg     "\n"
+      "MEMORY:  " memory  "\n"
+      "SHELL:   " shell   "\n"
+      "UPTIME:  " uptime  "\n"
+      )
      )
-  (display
-   (string-append
-    user "@" host       "\n"
-    "CPU:     " cpu     "\n"
-    "DESKTOP: " desktop "\n"
-    "DEVICE:  " device  "\n"
-    "DISTRO:  " distro  "\n"
-    "EDITOR:  " editor  "\n"
-    "KERNEL:  " kernel  "\n"
-    "PKGS:    " pkg     "\n"
-    "MEMORY:  " memory  "\n"
-    "SHELL:   " shell   "\n"
-    "UPTIME:  " uptime  "\n"
     )
-   )
   )
