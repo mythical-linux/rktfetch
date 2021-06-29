5a325eb08fc2f2643bcfd6e1ead36039b33d6193
Maciej Barć <xgqt@riseup.net>
2021-06-30 01:54:57 +0200

    editor.rkt: windows support
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- rktfetch/private/get/editor.rkt
+++ rktfetch/private/get/editor.rkt
@@ -5,12 +5,14 @@
 
 (require
  "helpers/basename.rkt"
+ "helpers/cmd.rkt"
+ (only-in "helpers/string.rkt" remove-strings)
  )
 
 (provide get-editor)
 
 
-(define (get-editor)
+(define (get-editor-unix)
   (let
       ([EDITOR (getenv "EDITOR")])
     (if EDITOR
@@ -18,4 +20,16 @@
         "N/A (could not read $EDITOR, make sure it is set)"
         )
     )
+  )
+
+(define (get-editor-windows)
+  (remove-strings (cmd->flat-str "ftype textfile") '("textfile=" "\""))
+  )
+
+
+(define (get-editor os)
+  (case os
+    [("unix")  (get-editor-unix)]
+    [("windows")  (get-editor-windows)]
+    [else "N/A (your OS isn't supported)"])
   )
