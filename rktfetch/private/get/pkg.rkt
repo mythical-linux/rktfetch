f9910b5148df9444c00d23434bd531ac3ca97c43
Maciej Barć <xgqt@riseup.net>
2021-06-30 20:57:51 +0200

    pkg.rkt: count number of packages
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- rktfetch/private/get/pkg.rkt
+++ rktfetch/private/get/pkg.rkt
@@ -5,7 +5,14 @@
 
 
 (require
- (only-in racket/string string-join)
+ (only-in racket/file find-files)
+ (only-in racket/format ~a)
+ (only-in racket/string
+          string-contains?
+          string-join
+          )
+ "helpers/basename.rkt"
+ "helpers/cmd.rkt"
  )
 
 (provide get-pkg)
@@ -16,17 +23,14 @@
       (
        [all  '#hash(
                     ["apk"        . "APK"]
-                    ["dnf"        . "DNF"]
                     ["dpkg"       . "DPKG"]
                     ["emerge"     . "Portage"]
                     ["guix"       . "Guix"]
                     ["nix-env"    . "Nix"]
                     ["pacman"     . "Pacman"]
-                    ["pkg"        . "PKG"]
+                    ["pkg_info"   . "PKG"]
                     ["rpm"        . "RPM"]
                     ["xbps-query" . "XBPS"]
-                    ["yum"        . "YUM"]
-                    ["zypper"     . "Zypper"]
                     )]
        [found '()]
        )
@@ -40,15 +44,77 @@
     )
   )
 
+(define (cnt-out cmd)
+  (cmd->flat-str (~a cmd " | wc -l"))
+  )
+
+(define (package-count-apk)
+  (cnt-out "apk info")
+  )
+
+(define (package-count-dpkg)
+  (cnt-out "dpkg-query -f '.\n' -W")
+  )
+
+(define (package-count-guix)
+  (cnt-out "guix package --list-installed")
+  )
+
+(define (package-count-nix)
+  (cnt-out "nix-store -q --requisites ~/.nix-profile")
+  )
+
+(define (package-count-pacman)
+  (cnt-out "pacman -Qq")
+  )
+
+(define (package-count-pkg)
+  (cnt-out "pkg_info")
+  )
+
+(define (package-count-portage)
+  (apply + (map
+            (λ (path)
+              (length (filter (lambda (subdir)
+                                (directory-exists? (build-path path subdir))
+                                )
+                              (directory-list path))))
+            (directory-list "/var/db/pkg" #:build? #t)
+            )
+         )
+  )
+
+(define (package-count-rpm)
+  (cnt-out "rpm -qa")
+  )
+
+(define (package-count-xbps)
+  (cnt-out "xbps-query -l")
+  )
+
+;; this is a very cool magic trick ;P
+(define ns (variable-reference->namespace (#%variable-reference)))
+
+(define-syntax-rule (package-count pm)
+  ((eval
+    (string->symbol (string-append "package-count-" (string-downcase pm)))
+    ns
+    ))
+  )
+
 (define (get-pkgmanager-unix)
   (let
       ([pms (get-pkgmanagers-unix)])
     (if (null? pms)
         "N/A (unknown package manager)"
-        (string-join pms ", ")
+        (string-join
+         (map (lambda (pm) (~a pm " (" (package-count pm) ")")) pms)
+         ", "
+         )
         )
     )
   )
+
 
 (define (get-pkg os)
   (case os
