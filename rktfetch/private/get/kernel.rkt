554271825643cf853f30cca14ae1ee17bc055b30
Maciej Barć <xgqt@riseup.net>
2021-07-02 08:45:39 +0200

    kernel.rkt: shorten
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- rktfetch/private/get/kernel.rkt
+++ rktfetch/private/get/kernel.rkt
@@ -4,26 +4,22 @@
 #lang racket/base
 
 (require
- (only-in racket/file file->string)
  "helpers/cmd.rkt"
- "helpers/string.rkt"
+ (only-in "helpers/grep.rkt" first-line)
+ (only-in "helpers/is.rkt" file-is?)
  )
 
 (provide get-kernel)
 
 
 (define (get-kernel-unix)
-  (let
-      ([linux-kernel-file "/proc/sys/kernel/osrelease"])
-    (cond
-      [(file-exists? linux-kernel-file)
-       (remove-newlines  (file->string linux-kernel-file))
-       ]
-      [(cmd->flat-str "uname -r")]
-      [else "N/A (could not read '/proc/sys/kernel/osrelease' nor could run 'uname')"]
-      )
+  (cond
+    [(file-is? "/proc/sys/kernel/osrelease") => first-line]
+    [(cmd->flat-str "uname -r")]
+    [else "N/A (could not read kernel)"]
     )
   )
+
 
 (define (get-kernel os)
   (case os
