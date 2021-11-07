75f01327096d656c8d867ccc94626a4efb20e839
Maciej Barć <xgqt@riseup.net>
2021-11-07 20:59:52 +0100

    rktfetch/info.rkt: add raco subcommand
    Closes: https://github.com/mythical-linux/rktfetch/issues/4
    
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- rktfetch/info.rkt
+++ rktfetch/info.rkt
@@ -10,6 +10,9 @@
 (define racket-launcher-names '("rktfetch"))
 (define racket-launcher-libraries '("main.rkt"))
 
+(define raco-commands
+  '(("rktfetch" (submod rktfetch main) "display system informatiom" #f)))
+
 (define scribblings
   '(("scribblings/rktfetch.scrbl"
      () (tool) "rktfetch")))
