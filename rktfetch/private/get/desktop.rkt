5d699932372977f1be7c7818513fd8303aa15b91
Maciej Barć <xgqt@gentoo.org>
2022-03-23 15:24:58 +0100

    rktfetch/private/get/desktop.rkt: format
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- rktfetch/private/get/desktop.rkt
+++ rktfetch/private/get/desktop.rkt
@@ -15,32 +15,25 @@
 
 
 (define (get-desktop-unix)
-  (let*
-      (
-       [xinitrc (or (getenv "XINITRC")
-                   (if (getenv "HOME")
-                       (string-append (getenv "HOME") "/.xinitrc")
-                       "/root/.xinitrc"
-                       )
-                   )]
-       )
+  (let* ([xinitrc (or (getenv "XINITRC")
+                     (if (getenv "HOME")
+                         (string-append (getenv "HOME") "/.xinitrc")
+                         "/root/.xinitrc"
+                         )
+                     )])
     (cond
       [(getenv "XDG_DESKTOP_SESSION")]
       [(getenv "XDG_CURRENT_DESKTOP")]
       [(getenv "DESKTOP_SESSION")]
       [(file-is? xinitrc)
        => (lambda (f)
-            (last (string-split (last (file->lines xinitrc)) " "))
-            )
-       ]
-      [else "N/A (could not get current desktop)"])
-    )
-  )
+            (last (string-split (last (file->lines xinitrc)) " ")))]
+      [else "N/A (could not get current desktop)"]
+      )))
 
 
 (define (get-desktop os)
   (case os
     [("unix")  (get-desktop-unix)]
     [else  "N/A (your OS isn't supported)"]
-    )
-  )
+    ))
